c0a29dffebec1e1e7c508f73ba66506f0c299257
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-20 16:45:58 +0600

    update url

--- src/api/client/private.client.js
+++ src/api/client/private.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 // const baseURL = "https://BertFlix-api.vercel.app/api/v1/";
-const baseURL = "http://localhost:5000/api/v1/";
+const baseURL = "https://movie-backend-hazel.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
